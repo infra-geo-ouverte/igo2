ade1e13170a3b029adccfec6aec910290a5e506c
Charles-Éric <charlesericbourget@gmail.com>
2017-01-26 15:52:15 -0500

    Remove header module (#37)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,8 +2,8 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
-import { HeaderModule } from './header/header.module';
 import { NavigatorPageModule } from './navigator-page/navigator-page.module';
 
 describe('AppComponent', () => {
@@ -11,7 +11,6 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HeaderModule,
         NavigatorPageModule
       ],
       declarations: [
@@ -31,12 +30,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('igo works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Igo-dev');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './shared/shared.module';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderModule } from './header/header.module';
 import { NavigatorPageModule } from './navigator-page/navigator-page.module';
 
 @NgModule({
@@ -15,7 +13,6 @@ import { NavigatorPageModule } from './navigator-page/navigator-page.module';
 
     SharedModule.forRoot(),
     AppRoutingModule,
-    HeaderModule,
     NavigatorPageModule
   ],
   providers: [],
