1bb3ead924281563362180d4cca0bf0a9abdad76
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2017-01-27 13:22:53 -0500

    I18n (#41)
    * add locale module

---
+++ src/i18n-providers.ts
@@ -0,0 +1,33 @@
+import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
+import {Config} from './config'
+
+export function getTranslationProviders(): Promise<Object[]> {
+  // Get the locale id from the global
+  //const locale = document['locale'] as string;
+  // return no providers if fail to get translation file for locale
+  const noProviders: Object[] = [];
+
+  // Define a way to retrieve the local information
+  const locale = Config.locale;
+
+  // No locale or U.S. English: no translation providers
+  if (!locale || locale === 'en-US') {
+    return Promise.resolve(noProviders);
+  }
+
+  // Ex: 'locale/messages.es.xlf`
+  const translationFile = `./locale/messages.${locale}.xlf`;
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', translationFile);
+    xhr.onload = (data: any) => resolve(
+      [
+        { provide: TRANSLATIONS, useValue: data.target.response },
+        { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+        { provide: LOCALE_ID, useValue: locale }
+      ]
+    );
+    xhr.onerror = () => reject(noProviders);
+    xhr.send();
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+import { getTranslationProviders } from './i18n-providers';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+getTranslationProviders().then(providers => {
+  const options = { providers };
+  platformBrowserDynamic().bootstrapModule(AppModule, options);
+});
