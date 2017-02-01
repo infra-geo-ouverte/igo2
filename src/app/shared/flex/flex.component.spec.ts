b67fa6cccfc883d2e17d9f01516c17ae74b5c9dc
Charles-Éric <charlesericbourget@gmail.com>
2017-02-01 13:18:10 -0500

    refactor(flex-pane) Refactor flex-pane (#58)
    * refactor(flex-pane) Refactor flex-pane

---
+++ src/app/shared/flex/flex.component.spec.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { FlexComponent } from './flex.component';
+
+describe('FlexComponent', () => {
+  let component: FlexComponent;
+  let fixture: ComponentFixture<FlexComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FlexComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FlexComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
