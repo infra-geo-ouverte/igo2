1d5b9c29efb41eae46717b7b69cbc92684cbc8c2
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2017-02-01 18:20:32 -0500

    test(e2e): add tests e2e for searchbar and navigator div

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -8,4 +8,15 @@ describe('igo App', function() {
   beforeEach(() => {
     page = new IgoPage();
   });
+
+  it('should have navigator div', () => {
+    page.navigateTo();
+    expect(page.getNavigatorDiv().isPresent()).toBe(true);
+  });
+
+  it('should display placeholder in search bar', () => {
+    page.navigateTo();
+    expect(page.getSearchBarInput().getAttribute('placeholder')).toEqual('Search for an address or a place');
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -4,4 +4,13 @@ export class IgoPage {
   navigateTo() {
     return browser.get('/');
   }
+
+  getNavigatorDiv() {
+    return element(by.css('igo-app igo-navigator'));
+  }
+
+  getSearchBarInput() {
+    return element(by.css('igo-app igo-search-bar input'));
+  }
+
 }
