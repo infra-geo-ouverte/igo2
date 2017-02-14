ab990d25fbadf104f90f36d8d8e819e9a1380c6b
cbourget <cbourget@mapgears.com>
2017-02-14 15:39:17 -0500

    Rename file and fix lint

--- src/app/core/search-source.service.ts
+++ src/app/core/search-source.service.ts
@@ -1,7 +1,4 @@
-import { Response, URLSearchParams } from '@angular/http';
-
 import { SearchSource } from '../search/sources/search-source';
-import { SearchResult } from '../search/shared/search-result.interface';
 
 export class SearchSourceService {
 
