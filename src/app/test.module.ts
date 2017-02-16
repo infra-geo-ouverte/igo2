aace23bef712d784e53ca415f43135e603618628
Charles-Éric <charlesericbourget@gmail.com>
2017-02-16 13:53:42 -0500

    Update test.module.ts

--- src/app/test.module.ts
+++ src/app/test.module.ts
@@ -4,16 +4,16 @@ import { HttpModule, Http } from '@angular/http';
 import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
 
 export function createTranslateLoader(http: Http) {
-    return new TranslateStaticLoader(http, './assets/locale', '.json');
+  return new TranslateStaticLoader(http, './assets/locale', '.json');
 }
 
 @NgModule({
   imports: [
     HttpModule,
     TranslateModule.forRoot({
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [Http]
+      provide: TranslateLoader,
+      useFactory: createTranslateLoader,
+      deps: [Http]
     }),
     MaterialModule.forRoot()
   ],
