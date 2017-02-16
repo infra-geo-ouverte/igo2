5eb31e753f892dbc8f85d041ee98b193d404a270
Charles-Éric <charlesericbourget@gmail.com>
2017-02-16 13:49:17 -0500

    Update missing-translation.guard.ts

--- src/app/core/language/missing-translation.guard.ts
+++ src/app/core/language/missing-translation.guard.ts
@@ -1,7 +1,7 @@
 import {MissingTranslationHandler, MissingTranslationHandlerParams} from 'ng2-translate';
 
 export class IgoMissingTranslationHandler implements MissingTranslationHandler {
-    handle(params: MissingTranslationHandlerParams) {
-        throw new Error(`The Key "${params.key}" is missing in locale file.`);
-    }
+  handle(params: MissingTranslationHandlerParams) {
+    throw new Error(`The Key "${params.key}" is missing in locale file.`);
+  }
 }
