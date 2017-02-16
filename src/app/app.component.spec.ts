8636795c276518a5be670edd8d02dfc48526c15e
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2017-02-16 11:33:13 -0500

    remove disabled no-unused-variable

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,3 @@
-/* tslint:disable:no-unused-variable */
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
--- src/app/core/search.service.spec.ts
+++ src/app/core/search.service.spec.ts
@@ -1,9 +1,7 @@
-/* tslint:disable:no-unused-variable */
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { SearchService } from './search.service';
 import { HttpModule, JsonpModule } from '@angular/http';
 
-import { searchResults } from '../reducers';
 import {
    provideAppStore,
    provideSearchSource,
