8d8588401636e681836eddbfeec78ddb331bd76c
Charles-Éric <charlesericbourget@gmail.com>
2017-02-16 13:53:02 -0500

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-import {TranslateModule} from 'ng2-translate';
+import { TranslateModule } from 'ng2-translate';
 
 import { BackdropComponent } from './backdrop/backdrop.component';
 import { FlexComponent } from './flex/flex.component';
