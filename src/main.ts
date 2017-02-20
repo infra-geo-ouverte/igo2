7684c10647a9c1cde220a208cbc0ccaedf9b77ac
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2017-02-20 18:02:58 -0500

    fix(protractor): retro version of protractor (#125)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-
 if (environment.production) {
   enableProdMode();
 }
