26d4a816711fcaf08ecf312e71238f24c0a9e007
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2017-04-03 10:04:00 -0400

    retro protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -20,6 +20,7 @@ var config = {
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
+  useAllAngular2AppRoots: true,
   beforeLaunch: function() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
