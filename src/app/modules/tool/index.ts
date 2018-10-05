681e8fb6b9c5ae26e241543acf831e37d37392f5
cbourget <charlesericbourget@gmail.com>
2018-10-05 13:20:58 -0400

    remove unused search results tool

--- src/app/modules/tool/index.ts
+++ src/app/modules/tool/index.ts
@@ -1,3 +1,2 @@
 export * from './edit-addresses-tool';
 export * from './search-client-tool';
-export * from './search-results-tool';
--- src/app/modules/tool/tool.module.ts
+++ src/app/modules/tool/tool.module.ts
@@ -2,15 +2,13 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 
 import { FadqSearchClientToolModule } from './search-client-tool/search-client-tool.module';
 import { FadqEditAddressesToolModule } from './edit-addresses-tool/edit-addresses-tool.module';
-import { FadqSearchResultsToolModule } from './search-results-tool/search-results-tool.module';
 
 @NgModule({
   imports: [],
   declarations: [],
   exports: [
     FadqSearchClientToolModule,
-    FadqEditAddressesToolModule,
-    FadqSearchResultsToolModule
+    FadqEditAddressesToolModule
   ]
 })
 export class FadqToolModule {
