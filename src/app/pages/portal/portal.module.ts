2d0d0963194f0a2590320471ff2f65c112e7e510
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-10-24 12:21:00 -0400

    fix(dropGeoFile): directive not working without dragAndDrop (build prod - aot)

--- src/app/pages/portal/portal.module.ts
+++ src/app/pages/portal/portal.module.ts
@@ -11,7 +11,8 @@ import { IgoCoreModule } from '@igo2/core';
 import {
   IgoPanelModule,
   IgoBackdropModule,
-  IgoFlexibleModule
+  IgoFlexibleModule,
+  IgoDrapDropModule
 } from '@igo2/common';
 import { IgoGeoModule } from '@igo2/geo';
 import { IgoContextModule } from '@igo2/context';
@@ -31,6 +32,7 @@ import { PortalComponent } from './portal.component';
     IgoBackdropModule,
     IgoFlexibleModule,
     IgoGeoModule,
+    IgoDrapDropModule,
     IgoContextModule,
     IgoToolsModule
   ],
