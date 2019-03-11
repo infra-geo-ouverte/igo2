a6a87e84738de998cb29336613b4e93759e6e415
Michael Lane <michael.lane@msp.gouv.qc.ca>
2019-03-11 13:26:19 -0400

    feat(mapOverlay): adapt to media change (#251)
    * mapOverlay adapt to media change
    
    * Unsubscribe media$

--- src/app/pages/portal/map-overlay/map-overlay.component.ts
+++ src/app/pages/portal/map-overlay/map-overlay.component.ts
@@ -13,6 +13,8 @@ import { MapOverlay } from './map-overlay.interface';
 export class MapOverlayComponent implements AfterViewInit, OnDestroy {
   public mapOverlay: MapOverlay[] = [];
   private context$$: Subscription;
+  private media$$: Subscription;
+  private context: Context;
 
   constructor(
     private contextService: ContextService,
@@ -21,13 +23,19 @@ export class MapOverlayComponent implements AfterViewInit, OnDestroy {
   ) { }
 
   ngAfterViewInit() {
-    this.context$$ = this.contextService.context$.subscribe(context =>
-      this.handleContextChange(context)
+    this.context$$ = this.contextService.context$.subscribe(context => {
+        this.handleContextChange(context);
+        this.context = context;
+        }
+    );
+    this.media$$ = this.mediaService.media$.subscribe(media =>
+      this.handleContextChange(this.context)
     );
   }
 
   ngOnDestroy() {
     this.context$$.unsubscribe();
+    this.media$$.unsubscribe();
   }
 
   private handleContextChange(context: Context) {
