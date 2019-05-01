b3c0fd59ea37136d4d14a4deb5583c78a3cd79b3
cbourget <charlesericbourget@gmail.com>
2019-05-01 16:06:36 -0400

    refactor(prod): fix prod env issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,12 +10,5 @@ interface Environment {
 export const environment: Environment = {
   production: true,
   configPath: './config/config.json',
-  igo: Object.assign({}, igoEnvironment, {
-    context: {
-      url: '/app/interne/igovisualisation/contexte_catalogue'
-    },
-    importExport: {
-      url: 'http://plssisigpro1.fadq.qc:3000'
-    }
-  })
+  igo: igoEnvironment
 };
