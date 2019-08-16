d87babc89ac2f28003122d6ea04b0f48e092aa99
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2019-08-16 16:45:10 -0400

    feat(search): serach order

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,16 +14,23 @@ export const environment: Environment = {
   igo: {
     searchSources: {
       nominatim: {
-        enabled: false
+        available: false
       },
       icherche: {
         searchUrl: '/apis/icherche/geocode',
+        order: 2,
         params: {
           limit: '8'
         }
       },
+      icherchereverse: {
+        searchUrl: '/apis/territoires/locate',
+        order: 3,
+        enabled: true
+      },
       ilayer: {
         searchUrl: '/apis/layers/search',
+        order: 4,
         params: {
           limit: '5'
         }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -27,22 +27,29 @@ export const environment: Environment = {
       tokenKey: 'id_token_igo',
       allowAnonymous: true
     },
+    context: {
+      url: "/apis/igo2",
+      defaultContextUri: "5"
+    },
     searchSources: {
       nominatim: {
         available: false
       },
       icherche: {
         searchUrl: '/apis/icherche/geocode',
+        order: 2,
         params: {
           limit: '8'
         }
       },
       icherchereverse: {
         searchUrl: '/apis/territoires/locate',
+        order: 3,
         enabled: true
       },
       ilayer: {
         searchUrl: '/apis/layers/search',
+        order: 4,
         params: {
           limit: '5'
         }
