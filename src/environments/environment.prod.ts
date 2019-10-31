d190be14a253bfbfa18d2cfe753a94c4e26de80e
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2019-10-31 13:07:11 -0400

    feat(config): add projections

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 import { LanguageOptions } from '@igo2/core';
-import { SearchSourceOptions, ImportExportServiceOptions } from '@igo2/geo';
+import {
+  SearchSourceOptions,
+  ImportExportServiceOptions,
+  Projection
+} from '@igo2/geo';
 
 interface Environment {
   production: boolean;
@@ -7,6 +11,7 @@ interface Environment {
     importExport?: ImportExportServiceOptions;
     language?: LanguageOptions;
     searchSources?: { [key: string]: SearchSourceOptions };
+    projections?: Projection[];
   };
 }
 
@@ -42,6 +47,20 @@ export const environment: Environment = {
           limit: '5'
         }
       }
-    }
+    },
+    projections: [
+      {
+        code: 'EPSG:32198',
+        def:
+          '+proj=lcc +lat_1=60 +lat_2=46 +lat_0=44 +lon_0=-68.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+        extent: [-799574, 45802, 891595.4, 1849567.5]
+      },
+      {
+        code: 'EPSG:3798',
+        def:
+          '+proj=lcc +lat_1=50 +lat_2=46 +lat_0=44 +lon_0=-70 +x_0=800000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+        extent: [31796.5834, 158846.2231, 1813323.4284, 2141241.0978]
+      }
+    ]
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,11 @@
 import { AuthOptions } from '@igo2/auth';
 import { ContextServiceOptions } from '@igo2/context';
 import { LanguageOptions } from '@igo2/core';
-import { SearchSourceOptions, CatalogServiceOptions } from '@igo2/geo';
+import {
+  SearchSourceOptions,
+  CatalogServiceOptions,
+  Projection
+} from '@igo2/geo';
 
 interface Environment {
   production: boolean;
@@ -16,6 +20,7 @@ interface Environment {
     context?: ContextServiceOptions;
     language?: LanguageOptions;
     searchSources?: { [key: string]: SearchSourceOptions };
+    projections?: Projection[];
   };
 }
 
@@ -71,6 +76,20 @@ export const environment: Environment = {
           limit: '5'
         }
       }
-    }
+    },
+    projections: [
+      {
+        code: 'EPSG:32198',
+        def:
+          '+proj=lcc +lat_1=60 +lat_2=46 +lat_0=44 +lon_0=-68.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+        extent: [-799574, 45802, 891595.4, 1849567.5]
+      },
+      {
+        code: 'EPSG:3798',
+        def:
+          '+proj=lcc +lat_1=50 +lat_2=46 +lat_0=44 +lon_0=-70 +x_0=800000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+        extent: [31796.5834, 158846.2231, 1813323.4284, 2141241.0978]
+      }
+    ]
   }
 };
