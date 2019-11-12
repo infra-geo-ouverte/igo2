46ebaa19e819fc005ccc5574d961d26dedebfbf1
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2019-11-12 12:26:58 -0500

    lint(environment): fix lines

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -81,13 +81,15 @@ export const environment: Environment = {
       {
         code: 'EPSG:32198',
         def:
-          '+proj=lcc +lat_1=60 +lat_2=46 +lat_0=44 +lon_0=-68.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+          '+proj=lcc +lat_1=60 +lat_2=46 +lat_0=44 +lon_0=-68.5 +x_0=0 +y_0=0 +ellps=GRS80 \
+          +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
         extent: [-799574, 45802, 891595.4, 1849567.5]
       },
       {
         code: 'EPSG:3798',
         def:
-          '+proj=lcc +lat_1=50 +lat_2=46 +lat_0=44 +lon_0=-70 +x_0=800000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+          '+proj=lcc +lat_1=50 +lat_2=46 +lat_0=44 +lon_0=-70 +x_0=800000 +y_0=0 \
+          +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
         extent: [31796.5834, 158846.2231, 1813323.4284, 2141241.0978]
       }
     ]
