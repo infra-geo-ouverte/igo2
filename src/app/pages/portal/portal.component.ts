a535e716f01e8aa58de547e1437b91de42f0b0d2
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2019-11-28 14:08:59 -0500

    ix(sharemap): Layers added by url now correctly interpret the visibility param from url (#360)
    * fix(sharemap)Layers added by url interpret the visibility param
    
    * refactor(portal) added layer visibility

--- src/app/pages/portal/portal.component.ts
+++ src/app/pages/portal/portal.component.ts
@@ -49,7 +49,9 @@ import {
   SearchSourceService,
   CapabilitiesService,
   sourceCanSearch,
-  sourceCanReverseSearch
+  sourceCanReverseSearch,
+  generateWMSIdFromSourceOptions,
+  WMSDataSourceOptions
 } from '@igo2/geo';
 
 import {
@@ -708,9 +710,13 @@ export class PortalComponent implements OnInit, OnDestroy {
         currentLayersByService = currentLayersByService.split(',');
         currentLayersByService.forEach(layer => {
           const layerFromUrl = layer.split(':igoz');
+          const layerOptions = {url: url, params: {LAYERS: layerFromUrl[0]}};
+          const id = generateWMSIdFromSourceOptions(layerOptions as WMSDataSourceOptions);
+          const visibility = this.computeLayerVisibilityFromUrl(params, id);
           this.addLayerByName(
             url,
             layerFromUrl[0],
+            visibility,
             parseInt(layerFromUrl[1] || 1000, 10)
           );
         });
@@ -719,7 +725,7 @@ export class PortalComponent implements OnInit, OnDestroy {
     }
   }
 
-  private addLayerByName(url: string, name: string, zIndex: number = 100000) {
+  private addLayerByName(url: string, name: string, visibility: boolean = true, zIndex: number = 100000) {
     if (!this.contextLoaded) {
       return;
     }
@@ -727,6 +733,7 @@ export class PortalComponent implements OnInit, OnDestroy {
       this.layerService
         .createAsyncLayer({
           zIndex: zIndex,
+          visible: visibility,
           sourceOptions: {
             optionsFromCapabilities: true,
             type: 'wms',
@@ -740,5 +747,43 @@ export class PortalComponent implements OnInit, OnDestroy {
         .subscribe(l => {
           this.map.addLayer(l);
         }));
+  }
+
+  private computeLayerVisibilityFromUrl(params: Params, currentLayerid: string): boolean {
+    const queryParams = params;
+    let visible = true;
+    if (!queryParams || !currentLayerid) {
+      return visible;
+    }
+    let visibleOnLayersParams = '';
+    let visibleOffLayersParams = '';
+    let visiblelayers: string[] = [];
+    let invisiblelayers: string[] = [];
+    if (queryParams['visiblelayers']) {
+      visibleOnLayersParams = queryParams['visiblelayers'];
+    }
+    if (queryParams['invisiblelayers']) {
+      visibleOffLayersParams = queryParams['invisiblelayers'];
+    }
+
+    /* This order is important because to control whichever
+     the order of * param. First whe open and close everything.*/
+    if (visibleOnLayersParams === '*') {
+      visible = true;
+    }
+    if (visibleOffLayersParams === '*') {
+      visible = false;
+    }
+
+    // After, managing named layer by id (context.json OR id from datasource)
+    visiblelayers = visibleOnLayersParams.split(',');
+    invisiblelayers = visibleOffLayersParams.split(',');
+    if (visiblelayers.indexOf(currentLayerid) > -1) {
+      visible = true;
+    }
+    if (invisiblelayers.indexOf(currentLayerid) > -1) {
+      visible = false;
+    }
+    return visible;
   }
 }
