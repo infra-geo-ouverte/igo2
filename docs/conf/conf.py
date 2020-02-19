8d6190f0feac87371c01815a216ca22da028c9b2
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2020-02-19 15:13:52 -0500

    wip docs

--- docs/conf/conf.py
+++ docs/conf/conf.py
@@ -1,10 +1,20 @@
 ## conf.py
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
+html_static_path = ['docs/conf/css']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
     'css/custom.css',
 ]
+
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The encoding of source files.
+#
+# source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = '../index'
