43c2ece697c599bbb10afefa0b0df88512db7f3e
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2020-02-19 15:03:11 -0500

    docs(docs) ajustement essai par css

---
+++ docs/conf/conf.py
@@ -0,0 +1,10 @@
+## conf.py
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
