106d66fcdccae68759c36b908eb79484aff28c67
Pierre-Étienne Lord <pe_lord@yahoo.ca>
2021-02-12 15:49:36 -0500

    wip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -45,10 +45,10 @@ export const environment: Environment = {
     catalog: {
       sources: [
         {
-          id: 'Arcgis Rest',
-          title: 'Arcgis Rest',
+          id: 'Image Arcgis Rest',
+          title: 'Image Arcgis Rest',
           url: 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer',
-          type: 'arcgisrest',
+          type: 'imagearcgisrest',
           sourceOptions: {
             queryable: true
           }
