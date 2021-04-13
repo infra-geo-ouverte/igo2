85f933f7409ff8cd5753ea8e31d659d002356e53
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2021-04-13 10:27:21 -0400

    config(overlayStyle): add overlayStyle config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,6 +20,15 @@ interface Environment {
     app: {
       forceCoordsNA: boolean;
     };
+    auth?: AuthOptions;
+    catalog?: CatalogServiceOptions;
+    context?: ContextServiceOptions;
+    importExport?: ImportExportServiceOptions;
+    language?: LanguageOptions;
+    searchSources?: { [key: string]: SearchSourceOptions };
+    projections?: Projection[];
+    interactiveTour?: { tourInMobile: boolean; pathToConfigFile: string };
+    depot?: { url: string };
     queryOverlayStyle?: {
       base?: CommonVectorStyleOptions,
       selection?: CommonVectorStyleOptions,
@@ -30,15 +39,6 @@ interface Environment {
       selection?: CommonVectorStyleOptions,
       focus?: CommonVectorStyleOptions
     };
-    auth?: AuthOptions;
-    catalog?: CatalogServiceOptions;
-    context?: ContextServiceOptions;
-    importExport?: ImportExportServiceOptions;
-    language?: LanguageOptions;
-    searchSources?: { [key: string]: SearchSourceOptions };
-    projections?: Projection[];
-    interactiveTour?: { tourInMobile: boolean; pathToConfigFile: string };
-    depot?: { url: string };
   };
 }
 
@@ -48,70 +48,6 @@ export const environment: Environment = {
     app: {
       forceCoordsNA: true
     },
-    searchOverlayStyle: {
-      base: {
-        markerColor: '#5ed0fb',          // marker fill
-        markerOpacity: 0.8,              // marker opacity not applied if a rgba markerColor is provided
-        markerOutlineColor: '#a7e7ff',   // marker contour
-        fillColor: '#5ed0fb',            // poly
-        fillOpacity: 0.2,                // poly fill opacity not applied if a rgba fillColor is provided
-        strokeColor: '#5ed0fb',          // line and poly
-        strokeOpacity: 0.7,              // line and poly not applied if a rgba strokeColor is provided
-        strokeWidth: 2                   // line and poly
-      },
-      focus: {
-        markerColor: '#5ed0fb',          // marker fill
-        markerOpacity: 1,                // marker opacity not applied if a rgba markerColor is provided
-        markerOutlineColor: '#DFF7FF',   // marker contour
-        fillColor: '#5ed0fb',            // poly
-        fillOpacity: 0.3,                // poly fill opacity not applied if a rgba fillColor is provided
-        strokeColor: '#DFF7FF',          // line and poly
-        strokeOpacity: 1,                // line and poly not applied if a rgba strokeColor is provided
-        strokeWidth: 2                   // line and poly
-      },
-      selection: {
-        markerColor: '#00a1de',           // marker fill
-        markerOpacity: 1,                 // marker opacity not applied if a rgba markerColor is provided
-        markerOutlineColor: '#ffffff',    // marker contour
-        fillColor: '#00a1de',             // poly
-        fillOpacity: 0.3,                 // poly fill opacity not applied if a rgba fillColor is provided
-        strokeColor: '#00A1DE',           // line and poly
-        strokeOpacity: 1,                 // line and poly not applied if a rgba strokeColor is provided
-        strokeWidth: 2                    // line and poly
-      }
-    },
-    queryOverlayStyle: {
-      base: {
-        markerColor: '#5ed0fb',          // marker fill
-        markerOpacity: 0.8,              // marker opacity not applied if a rgba markerColor is provided
-        markerOutlineColor: '#a7e7ff',   // marker contour
-        fillColor: '#5ed0fb',            // poly
-        fillOpacity: 0.2,                // poly fill opacity not applied if a rgba fillColor is provided
-        strokeColor: '#5ed0fb',          // line and poly
-        strokeOpacity: 0.7,              // line and poly not applied if a rgba strokeColor is provided
-        strokeWidth: 2                   // line and poly
-      },
-      focus: {
-        markerColor: '#5ed0fb',          // marker fill
-        markerOpacity: 1,                // marker opacity not applied if a rgba markerColor is provided
-        markerOutlineColor: '#DFF7FF',   // marker contour
-        fillColor: '#5ed0fb',            // poly
-        fillOpacity: 0.3,                // poly fill opacity not applied if a rgba fillColor is provided
-        strokeColor: '#DFF7FF',          // line and poly
-        strokeOpacity: 1,                // line and poly not applied if a rgba strokeColor is provided
-        strokeWidth: 2                   // line and poly
-      },
-      selection: {
-        markerColor: '#00a1de',           // marker fill
-        markerOpacity: 1,                 // marker opacity not applied if a rgba markerColor is provided
-        markerOutlineColor: '#ffffff',    // marker contour
-        fillColor: '#00a1de',             // poly
-        fillOpacity: 0.3,                 // poly fill opacity not applied if a rgba fillColor is provided
-        strokeColor: '#00A1DE',           // line and poly
-        strokeOpacity: 1,                 // line and poly not applied if a rgba strokeColor is provided
-        strokeWidth: 2                    // line and poly
-      }
-    },
     auth: {
       url: '/apis/users',
       tokenKey: 'id_token_igo',
@@ -119,7 +55,7 @@ export const environment: Environment = {
       trustHosts: ['geoegl.msp.gouv.qc.ca'],
     },
     catalog: {
-      sources: [               
+      sources: [
         {
           id: 'Image Arcgis Rest',
           title: 'Image Arcgis Rest',
@@ -340,6 +276,70 @@ export const environment: Environment = {
           +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
         extent: [31796.5834, 158846.2231, 1813323.4284, 2141241.0978]
       }
-    ]
+    ],
+    searchOverlayStyle: {
+      base: {
+        markerColor: '#5ed0fb',          // marker fill
+        markerOpacity: 0.8,              // marker opacity not applied if a rgba markerColor is provided
+        markerOutlineColor: '#a7e7ff',   // marker contour
+        fillColor: '#5ed0fb',            // poly
+        fillOpacity: 0.2,                // poly fill opacity not applied if a rgba fillColor is provided
+        strokeColor: '#5ed0fb',          // line and poly
+        strokeOpacity: 0.7,              // line and poly not applied if a rgba strokeColor is provided
+        strokeWidth: 2                   // line and poly
+      },
+      focus: {
+        markerColor: '#5ed0fb',          // marker fill
+        markerOpacity: 1,                // marker opacity not applied if a rgba markerColor is provided
+        markerOutlineColor: '#DFF7FF',   // marker contour
+        fillColor: '#5ed0fb',            // poly
+        fillOpacity: 0.3,                // poly fill opacity not applied if a rgba fillColor is provided
+        strokeColor: '#DFF7FF',          // line and poly
+        strokeOpacity: 1,                // line and poly not applied if a rgba strokeColor is provided
+        strokeWidth: 2                   // line and poly
+      },
+      selection: {
+        markerColor: '#00a1de',           // marker fill
+        markerOpacity: 1,                 // marker opacity not applied if a rgba markerColor is provided
+        markerOutlineColor: '#ffffff',    // marker contour
+        fillColor: '#00a1de',             // poly
+        fillOpacity: 0.3,                 // poly fill opacity not applied if a rgba fillColor is provided
+        strokeColor: '#00A1DE',           // line and poly
+        strokeOpacity: 1,                 // line and poly not applied if a rgba strokeColor is provided
+        strokeWidth: 2                    // line and poly
+      }
+    },
+    queryOverlayStyle: {
+      base: {
+        markerColor: '#5ed0fb',          // marker fill
+        markerOpacity: 0.8,              // marker opacity not applied if a rgba markerColor is provided
+        markerOutlineColor: '#a7e7ff',   // marker contour
+        fillColor: '#5ed0fb',            // poly
+        fillOpacity: 0.2,                // poly fill opacity not applied if a rgba fillColor is provided
+        strokeColor: '#5ed0fb',          // line and poly
+        strokeOpacity: 0.7,              // line and poly not applied if a rgba strokeColor is provided
+        strokeWidth: 2                   // line and poly
+      },
+      focus: {
+        markerColor: '#5ed0fb',          // marker fill
+        markerOpacity: 1,                // marker opacity not applied if a rgba markerColor is provided
+        markerOutlineColor: '#DFF7FF',   // marker contour
+        fillColor: '#5ed0fb',            // poly
+        fillOpacity: 0.3,                // poly fill opacity not applied if a rgba fillColor is provided
+        strokeColor: '#DFF7FF',          // line and poly
+        strokeOpacity: 1,                // line and poly not applied if a rgba strokeColor is provided
+        strokeWidth: 2                   // line and poly
+      },
+      selection: {
+        markerColor: '#00a1de',           // marker fill
+        markerOpacity: 1,                 // marker opacity not applied if a rgba markerColor is provided
+        markerOutlineColor: '#ffffff',    // marker contour
+        fillColor: '#00a1de',             // poly
+        fillOpacity: 0.3,                 // poly fill opacity not applied if a rgba fillColor is provided
+        strokeColor: '#00A1DE',           // line and poly
+        strokeOpacity: 1,                 // line and poly not applied if a rgba strokeColor is provided
+        strokeWidth: 2                    // line and poly
+      }
+    }
   }
 };
