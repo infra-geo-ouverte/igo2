8617d26b67d96dcb21631ab22565f08601d23c69
Pierre-Étienne Lord <7397743+pelord@users.noreply.github.com>
2021-06-14 17:32:09 -0400

    Pwa (#641)
    * wip
    
    * docs(properties): add maxLayerZoomExtent property to map (#633)
    
    * doc layerExtent
    
    * doc layerExtent
    
    * chore(polyfills): Error catching by textdecoder() was undefined on IE (#638)
    
    * chore(polyfills): Error catching by textdecoder() was undefined on IE
    
    * lint
    
    * wip
    
    * wip
    
    Co-authored-by: Maxime Lamer <83651899+LAMM26@users.noreply.github.com>

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -27,6 +27,9 @@ import 'core-js/es/array/flat-map';
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js'; // Run `npm install --save classlist.js`.
 
+/** IE do not support TextDecoder() */
+import 'fast-text-encoding';
+
 /** IE10 and IE11 requires the following for string normalization */
 import 'unorm/lib/unorm.js';
 
