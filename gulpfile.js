47ddafa7c3c3e878b08242552edc3140c198fa46
Pierre-Etienne Lord <pe_lord@pm.me>
2021-09-13 11:08:57 -0400

    chore(*): app migration to ng 12

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,7 @@
 /*jshint esversion: 6 */
 const gulp = require('gulp');
 const jeditor = require('gulp-json-editor');
-const babel = require('gulp-babel');
-const replace = require('gulp-replace');
-const concat = require('gulp-concat');
 
-gulp.task('concatStyles', done => {
-  gulp
-    .src([
-      './node_modules/@igo2/core/style/index.theming.scss',
-      './src/style/reset.scss',
-      './src/app/app.theming.scss',
-      './src/app/pages/portal/portal.theming.scss',
-      './src/app/pages/portal/sidenav/sidenav.theming.scss',
-      './src/app/pages/portal/welcome-window/welcome-window.theming.scss'
-    ])
-    .pipe(concat('theme.scss'))
-    .pipe(gulp.dest('./src/style', { overwrite: true }));
-
-  done();
-});
 
 gulp.task('geo:fixOL', done => {
   gulp
