7c0e493093961289aef83ee8fda02f9ca33627aa
Pierre-Etienne Lord <pe_lord@pm.me>
2021-09-16 15:07:20 -0400

    chore(gulp): remove unused process

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,4 @@
 /*jshint esversion: 6 */
-const gulp = require('gulp');
-const jeditor = require('gulp-json-editor');
 
 
-gulp.task('geo:fixOL', done => {
-  gulp
-    .src(['./node_modules/ol/package.json'])
-    .pipe(
-      jeditor({
-        sideEffects: true
-      })
-    )
-    .pipe(gulp.dest('./node_modules/ol/'));
 
-  done();
-});
