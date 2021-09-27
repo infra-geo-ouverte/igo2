73ddb699e1b5a8fe94d816ef87f1fae8397adfc4
Philippe Lafreniere <philippe.lafreniere@msp.gouv.qc.ca>
2021-09-27 11:10:01 -0400

    refactor(portal): menu button component to igo2-lib

--- src/app/pages/portal/portal.component.ts
+++ src/app/pages/portal/portal.component.ts
@@ -125,8 +125,6 @@ export class PortalComponent implements OnInit, OnDestroy {
   };
   public workspaceMenuClass = 'workspace-menu';
 
-  public menuButtonClass;
-
   public fullExtent = this.storageService.get('fullExtent') as boolean;
   private workspaceMaximize$$: Subscription[] = [];
   readonly workspaceMaximize$: BehaviorSubject<boolean> = new BehaviorSubject(
@@ -159,8 +157,6 @@ export class PortalComponent implements OnInit, OnDestroy {
     undefined
   );
   private routeParams: Params;
-  public menuButtonReverseColor = false;
-  public menuButtonStyle = {'color': 'primary', 'background-color': 'white'};
   public toastPanelHtmlDisplay = false;
 
   @ViewChild('mapBrowser', { read: ElementRef, static: true })
@@ -330,15 +326,6 @@ export class PortalComponent implements OnInit, OnDestroy {
     if (this.igoSearchPointerSummaryEnabled === undefined) {
       this.igoSearchPointerSummaryEnabled = this.storageService.get('searchPointerSummaryEnabled') as boolean || false;
     }
-
-    if (
-      typeof this.configService.getConfig('menuButtonReverseColor') !==
-      'undefined'
-    ) {
-      this.menuButtonReverseColor = this.configService.getConfig(
-        'menuButtonReverseColor'
-      );
-    }
   }
 
   ngOnInit() {
@@ -450,10 +437,6 @@ export class PortalComponent implements OnInit, OnDestroy {
           this.openSidenav();
           this.toolState.openSidenav$.next(false);
         }
-        this.menuButtonClass = this.getClassMenuButton();
-        if (this.menuButtonReverseColor) {
-          this.menuButtonStyle = {'color': 'white', 'background-color': 'primary'};
-        }
       }
     );
 
@@ -474,20 +457,6 @@ export class PortalComponent implements OnInit, OnDestroy {
       this.toastPanelOffsetX$.next(undefined);
     } else {
       this.toastPanelOffsetX$.next(this.getToastPanelExtent());
-    }
-  }
-
-  getClassMenuButton() {
-    if (this.sidenavOpened) {
-      return {
-        'menu-button': this.menuButtonReverseColor === false,
-        'menu-button-reverse-color': this.menuButtonReverseColor === true
-      };
-    } else {
-      return {
-        'menu-button': this.menuButtonReverseColor === false,
-        'menu-button-reverse-color-close': this.menuButtonReverseColor === true
-      };
     }
   }
 
