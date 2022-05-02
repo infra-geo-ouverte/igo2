4d705270b27889551ca2ba128b3aa704749d347c
Josée Martel <josee.martel@mffp.gouv.qc.ca>
2022-05-02 11:01:03 -0400

    wks_clearEntityOnContextChange

--- src/app/pages/portal/portal.component.ts
+++ src/app/pages/portal/portal.component.ts
@@ -750,6 +750,10 @@ export class PortalComponent implements OnInit, OnDestroy {
     if (context === undefined) {
       return;
     }
+    if (this.workspace && !this.workspace.entityStore.empty) {
+      this.workspace.entityStore.clear();
+    }
+    
     if (!this.queryState.store.empty) {
       this.queryState.store.softClear();
     }
