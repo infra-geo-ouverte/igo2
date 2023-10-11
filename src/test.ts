e6a4ff56fa577b23198adb9ddb70b4c5cb6f0dfe
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2023-10-11 10:57:09 -0400

    fix(Prettier): ignore test file Need to keep the zone-js/testing at the top

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,5 @@
+import 'zone.js/dist/zone-testing';
+
 // This file is required by karma.conf.js and loads recursively all the .spec
 // and framework files
 import { getTestBed } from '@angular/core/testing';
@@ -5,8 +7,6 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
-import 'zone.js/dist/zone-testing';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
