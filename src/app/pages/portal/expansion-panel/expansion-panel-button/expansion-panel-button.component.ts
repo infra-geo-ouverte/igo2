e4537f29304249d8087103fc8051ecc74cfdaa4e
Aziz <119948730+aziz-access@users.noreply.github.com>
2023-11-28 16:42:40 -0500

    fix(app): replace tablet view by medium and small screen + map actions  button placement
    * fix(geo): replace talette view by medium and small screen
    
    * fix(map): remove empty space for map button
    
    * fix(portal): add spacer for horizontal
    
    * fix(map): map actions position
    
    * fix(test)
    
    ---------
    
    Co-authored-by: Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>

---
+++ src/app/pages/portal/expansion-panel/expansion-panel-button/expansion-panel-button.component.ts
@@ -0,0 +1,30 @@
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { IgoLanguageModule } from '@igo2/core';
+
+@Component({
+  selector: 'app-expansion-panel-button',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    IgoLanguageModule,
+    NgIf
+  ],
+  templateUrl: './expansion-panel-button.component.html',
+  styleUrls: ['./expansion-panel-button.component.scss']
+})
+export class ExpansionPanelButtonComponent {
+  @Input() expanded: boolean;
+
+  @Output() expand = new EventEmitter<boolean>();
+
+  onToggleClick(): void {
+    this.expand.emit(!this.expanded);
+  }
+}
