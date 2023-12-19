0bba88037ae6445b958f0855d2d1bcdf304c18a4
alecarn <133774929+alecarn@users.noreply.github.com>
2023-12-19 15:01:25 -0500

    feat: upgrade Angular v17

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,35 +1,7 @@
-/**
- * This file includes polyfills needed by Angular and is loaded before the app.
- * You can add your own extra polyfills to this file.
- *
- * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
- *
- * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
- *
- * Learn more in https://angular.io/guide/browser-support
- */
-
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */
-import 'core-js/es/array/flat-map';
-import 'core-js/es/array/includes';
-import 'core-js/es/object';
-// Run `npm install --save classlist.js`.
-
-/** Evergreen browsers require these. **/
-import 'core-js/es/reflect';
-import 'core-js/es/string';
-
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';
+import 'zone.js';
 
 // Included with Angular CLI.
 
