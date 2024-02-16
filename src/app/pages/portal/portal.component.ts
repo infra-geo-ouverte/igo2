d2f88f5f5980e4934ac5c5456780d20ac05fa3e6
Pierre-Étienne Lord <pierre-etienne.lord@transports.gouv.qc.ca>
2024-02-16 14:27:24 -0500

    feat(portal): try to detect epsg code from vector added by url
    refactor(geo): refactor based on review

--- src/app/pages/portal/portal.component.ts
+++ src/app/pages/portal/portal.component.ts
@@ -64,6 +64,7 @@ import {
   WfsWorkspace,
   addStopToStore,
   computeOlFeaturesExtent,
+  detectFileEPSG,
   featureFromOl,
   featureToSearchResult,
   generateIdFromSourceOptions,
@@ -91,7 +92,14 @@ import type { default as OlGeometry } from 'ol/geom/Geometry';
 import * as olProj from 'ol/proj';
 
 import { BehaviorSubject, Subscription, combineLatest, of } from 'rxjs';
-import { debounceTime, first, pairwise, skipWhile, take } from 'rxjs/operators';
+import {
+  concatMap,
+  debounceTime,
+  first,
+  pairwise,
+  skipWhile,
+  take
+} from 'rxjs/operators';
 import { getAppVersion } from 'src/app/app.utils';
 import { EnvironmentOptions } from 'src/environments/environnement.interface';
 
@@ -1443,10 +1451,21 @@ export class PortalComponent implements OnInit, OnDestroy {
           type: data.type,
           lastModified: Date.now()
         });
-        this.importService.import(file).subscribe(
-          (features: Feature[]) => this.onFileImportSuccess(file, features),
-          (error: Error) => this.onFileImportError(file, error)
-        );
+        detectFileEPSG({ file })
+          .pipe(
+            skipWhile((code) => !code),
+            first(),
+            concatMap((epsgCode) => {
+              return this.importService.import(
+                file,
+                epsgCode === 'epsgNotDefined' ? undefined : epsgCode
+              );
+            })
+          )
+          .subscribe(
+            (features: Feature[]) => this.onFileImportSuccess(file, features),
+            (error: Error) => this.onFileImportError(file, error)
+          );
       });
     }
   }
