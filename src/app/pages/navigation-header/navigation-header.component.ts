da55b4157ae67426551cefd34ab8ba44db404feb
fs-repos <156995325+fs-repos@users.noreply.github.com>
2024-08-04 15:16:46 +0300

    Update

--- src/app/pages/navigation-header/navigation-header.component.ts
+++ src/app/pages/navigation-header/navigation-header.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Observable, Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-navigation-header',
   templateUrl: './navigation-header.component.html',
   styleUrls: ['./navigation-header.component.scss']
 })
-export class NavigationHeaderComponent {
+export class NavigationHeaderComponent implements OnInit, OnDestroy {
   more = false;
+  resizeObservable$: Observable<Event>;
+  resizeSubscription$: Subscription;
+  mobileView = false;
 
   onToggleMore() {
     this.more = !this.more;
+  }
+
+  ngOnInit(): void {
+    this.resizeObservable$ = fromEvent(window, 'resize');
+    this.resizeSubscription$ = this.resizeObservable$.subscribe((e) => {
+      if (window.innerWidth <= 400) {
+        this.renderMobileView();
+      } else {
+        this.renderDesktopView();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.resizeSubscription$.unsubscribe();
+  }
+
+  renderMobileView() {
+    console.log('render mobile view');
+    this.mobileView = true;
+    this.more = false;
+  }
+
+  renderDesktopView() {
+    console.log('render desktop view');
+    this.mobileView = false;
+    this.more = false;
   }
 }
