90464dabb2fab296574bc201972dc236c4a7fc56
fs-repos <156995325+fs-repos@users.noreply.github.com>
2024-08-19 10:26:41 +0300

    Update

--- src/app/pages/carte/carte.component.ts
+++ src/app/pages/carte/carte.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
 
-import { ConfigService } from '@igo2/core';
-
 @Component({
   selector: 'app-carte',
   templateUrl: './carte.component.html',
   styleUrls: ['./carte.component.scss']
 })
-export class CarteComponent {
-  public hasHeader: boolean;
-  public hasNavigationHeader: boolean;
-  public hasFooter: boolean;
-
-  constructor(private configService: ConfigService) {
-    this.hasHeader = this.configService.getConfig('header.hasHeader', false);
-    this.hasNavigationHeader = this.configService.getConfig(
-      'header.hasNavigationHeader',
-      false
-    );
-    this.hasFooter = this.configService.getConfig('hasFooter', false);
-  }
-}
+export class CarteComponent {}
