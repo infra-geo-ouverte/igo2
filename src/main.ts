438708ed8b2024ee50365dee691735cc567200da
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2024-11-12 10:08:46 -0500

    fix(core): language add static/async loader

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ import { withMicrosoftSupport } from '@igo2/auth/microsoft';
 import { provideIcon } from '@igo2/common/icon';
 import { IgoCoreModule } from '@igo2/core';
 import { ConfigService, provideConfig } from '@igo2/core/config';
-import { provideTranslation } from '@igo2/core/language';
+import { provideTranslation, withAsyncConfig } from '@igo2/core/language';
 import { IgoMessageModule } from '@igo2/core/message';
 import { RouteService } from '@igo2/core/route';
 import { loadTheme } from '@igo2/utils';
@@ -65,7 +65,7 @@ bootstrapApplication(AppComponent, {
       default: environment.igo,
       path: './config/config.json'
     }),
-    provideTranslation(),
+    provideTranslation(withAsyncConfig()),
     provideAuthentification(
       withMicrosoftSupport('add'),
       withMicrosoftSupport('b2c')
