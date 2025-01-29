205a3712a259c31861b55e4b25c0d23333feb28b
eliashoungbasse <houngbasseelias@gmail.com>
2025-01-29 11:41:13 +0000

    immeuble page v4, app-paginator

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -6,37 +6,68 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./paginator.component.css']
 })
 export class PaginatorComponent implements OnInit {
-  _limit: number = 0;
-  _skip: number = 0;
+  _limit: number = 10;
   _total: number = 0;
-  _pages = 0;
-  pagesArr = new Array(0);
-  currentPage = 0;
-  pagesFlag = false;
-  pagesStart = 0;
+  _pages: number = 0;
+  currentPage: number = 0;
+  pagesArr: number[] = []; //tableau contenant les numéros des pages affichées
 
   @Input() set limit(value: number) {
     this._limit = value;
-  }
-  @Input() set skip(value: number) {
-    this._skip = value;
+    this.calculatePages();
   }
   @Input() set total(value: number) {
     this._total = value;
-  }
-  @Input() set pages(value: number) {
-    this._pages = value;
+    this.calculatePages();
   }
 
   @Output() page = new EventEmitter<number>();
-  @Output() pageSize = new EventEmitter<number>();
+
+  constructor() {}
 
   ngOnInit(): void {
-    console.log();
+    this.calculatePages();
   }
 
-  onPage(a: any) {
-    this.page.next(a.pageIndex);
-    if (a.pageSize !== this._limit) this.pageSize.next(a.pageSize);
+  calculatePages() {
+    this._pages = Math.ceil(this._total / this._limit); // Arrondi supérieur pour obtenir le nombre de pages
+    this.updateVisiblePages(); // Met à jour les pages visibles
+  }
+
+  // Met à jour la liste des pages visibles en fonction de la page actuelle.
+  updateVisiblePages() {
+    let start = Math.max(0, this.currentPage - 1); // Décale la fenêtre de pagination
+    let end = Math.min(this._pages, start + 3); // Toujours afficher 3 pages maximum
+    this.pagesArr = Array.from(
+      { length: end - start },
+      (_, i) => start + i + 1
+    ); // Remplit pagesArr avec au maximum 3 numéros de pages visibles
+
+    this.pagesArr = this.pagesArr.filter(
+      (page) => page > 0 && page <= this._pages
+    ); // Assurer que la page est valide
+  }
+
+  goToPage(index: number) {
+    if (index < 0 || index >= this._pages) return;
+    this.currentPage = index;
+    this.page.emit(this.currentPage);
+    this.updateVisiblePages();
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.page.emit(this.currentPage);
+      this.updateVisiblePages();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this._pages - 1) {
+      this.currentPage++;
+      this.page.emit(this.currentPage);
+      this.updateVisiblePages();
+    }
   }
 }
