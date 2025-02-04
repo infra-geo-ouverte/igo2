44bbc916c1c70406225a1adea737810ed323e624
eliashoungbasse <houngbasseelias@gmail.com>
2025-02-04 14:24:08 +0000

    modification titre immeuble card

--- src/app/components/immeuble-card/immeuble-card.component.ts
+++ src/app/components/immeuble-card/immeuble-card.component.ts
@@ -7,4 +7,16 @@ import { Component, Input } from '@angular/core';
 })
 export class ImmeubleCardComponent {
   @Input() item: any;
+
+  formatTitle(item: any): string {
+    if (!item.adresse_immeuble) return '';
+
+    let partis = item.adresse_immeuble.split(',').map((p) => p.trim());
+
+    if (partis.length >= 4) {
+      return `${partis[0]}, ${partis[1]}\n${partis[2]} (${partis[3]}) ${partis.slice(4).join(' ')}`;
+    }
+
+    return item.adresse_immeuble; // Retourne l'adresse originale si elle ne suit pas le format attendu
+  }
 }
