968c32c3629556d48ef8a15ddec6c1c7428e7a69
eliashoungbasse <houngbasseelias@gmail.com>
2025-02-05 15:42:21 +0000

    modification fil d'ariane mobile

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { BreadcrumbService } from 'src/app/services/breadcrumb.service';
 
@@ -10,9 +10,38 @@ import { BreadcrumbService } from 'src/app/services/breadcrumb.service';
 export class BreadcrumbComponent implements OnInit {
   items: { label: string; url: string }[] = [];
 
+  isMobile = false;
+  displayedBreadcrumb: { label: string; url: string } | null = null;
+
   constructor(private readonly breadcrumbService: BreadcrumbService) {}
 
   ngOnInit(): void {
-    this.breadcrumbService.items.subscribe((items) => (this.items = items));
+    this.breadcrumbService.items.subscribe((items) => {
+      this.items = items;
+      this.updateBreadcrumbDisplay();
+    });
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  checkScreenSize(): void {
+    this.isMobile = window.innerWidth <= 768; // Détecte un écran mobile
+    this.updateBreadcrumbDisplay();
+  }
+
+  updateBreadcrumbDisplay(): void {
+    if (this.isMobile && this.items.length >= 2) {
+      if (
+        this.items.length >= 3 &&
+        this.items[this.items.length - 2].label ===
+          "Outil de repérage d'immeubles"
+      ) {
+        this.displayedBreadcrumb = this.items[this.items.length - 3];
+      } else {
+        this.displayedBreadcrumb = this.items[this.items.length - 2];
+      }
+    } else {
+      this.displayedBreadcrumb = null; // Afficher normalement sur desktop
+    }
   }
 }
