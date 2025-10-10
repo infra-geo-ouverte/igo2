09610211657f189b7b84550828faee29c3628cdf
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2025-10-10 13:59:05 -0400

    fix: update igo2-lib deps to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,12 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 import { AppEnvironmentOptions } from './environnement.interface';
+import { version } from './version';
 
 export const environment: AppEnvironmentOptions = {
   production: false,
   igo: {
+    version,
     app: {
       forceCoordsNA: true,
       install: {
